da5ee5867a6eca26aa4042e3be37d5ede35a73a6
cfrost <cassandra.e.hutchings@gmail.com>
2023-04-18 10:20:24 -0600

    final commit

--- app.js
+++ app.js
@@ -6,26 +6,82 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = list();
-  res.send(`<html>
+  res.send(`<!DOCTYPE html>
+  <html>
   <head>
-    <link rel="stylesheet" href="/style.css" />
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-    <h1>Wizard News</h1>
-    <ul>
-   ${posts
-     .map((post) => {
-       return `<li>${post.title}</li>`;
-     })
-     .join("")}
-
-    </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
 </html>`);
 });
 
-const PORT = 1337;
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = find(id);
+  
+  if (!post.id) {
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+    res.send(html);
+    return;
+  } else {
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+            ${post.content}
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`);
+}});
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
